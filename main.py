ba061867ed33ffed5b372344b4ad4618bdc5a6f3
dudleydehenau <skydrmddh@gmail.com>
2023-12-09 23:02:55 +0100

    Amélioration de la commande interractive grâce au module inquirer et ajout d'une option permettant d'afficher tout les fichiers d'un répertorie (Et option permettant de choisir d'afficher uniquement certaine extentions)

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import os
 import shutil
+from inquirer import prompt, List, Confirm
 
 class FileSorter:
-    def __init__(self, source_dir, update_folder=False):
+    def __init__(self, source_dir):
         self.source_dir = source_dir
-        self.update_folder = update_folder
+        self.extension_folders_created = False
 
     def create_extension_folders(self):
         # Créer des dossiers pour chaque extension dans le répertoire source
-        for file in os.listdir(self.source_dir):
-            if os.path.isfile(os.path.join(self.source_dir, file)):
-                extension = file.split('.')[-1].lower()
-                extension_folder = os.path.join(self.source_dir, extension)
-                if not os.path.exists(extension_folder):
-                    os.makedirs(extension_folder)
+        if not self.extension_folders_created:
+            for file in os.listdir(self.source_dir):
+                if os.path.isfile(os.path.join(self.source_dir, file)):
+                    extension = file.split('.')[-1].lower()
+                    extension_folder = os.path.join(self.source_dir, extension)
+                    if not os.path.exists(extension_folder):
+                        os.makedirs(extension_folder)
+            self.extension_folders_created = True
 
     def move_files(self):
         # Déplacer les fichiers dans les dossiers correspondants aux extensions
@@ -37,31 +40,39 @@ class FileSorter:
                 shutil.move(file_path, destination_path)
                 print(f"Moved: {file} to 'A Mettre A Jour' folder")
 
+    def filter_files_by_extension(self, file_types=None):
+        for file in os.listdir(self.source_dir):
+            if os.path.isfile(os.path.join(self.source_dir, file)):
+                if not file_types or any(file.lower().endswith(f".{ext}") for ext in file_types):
+                    print(file)
+
 def main():
-    # Demander à l'utilisateur d'entrer le chemin du répertoire source
-    source_dir = input("Entrez le chemin du répertoire source à trier (exemple : C:\Chemin\Repertoire) : ")
+    questions = [
+        List('action',
+             message="Choisissez une action",
+             choices=['1. Filtrer les fichiers par extension', '2. Donner la liste des fichiers'],
+        ),
+    ]
 
-    # Vérifier si le répertoire source existe
-    if not os.path.exists(source_dir):
-        print(f"Erreur: Le repertoire spécifié '{source_dir}' semble ne pas exister.")
-        return
+    answers = prompt(questions)
 
-    # Demander à l'utilisateur s'il veut déplacer les fichiers de moins de 1 Go vers "A Mettre A Jour"
-    update_folder = input("Voulez-vous déplacer les fichiers de moins de 1 Go vers 'A Mettre A Jour'? (Oui/Non): ").lower()
-    update_folder = update_folder == "oui"
+    source_dir = input("Entrez le chemin du répertoire source : ")
 
-    # Instancier la classe FileSorter
-    file_sorter = FileSorter(source_dir, update_folder)
+    if "1. Filtrer les fichiers par extension" in answers['action']:
+        file_sorter = FileSorter(source_dir)
+        move_large_files_question = Confirm('move_large_files',
+                                            message="Voulez-vous mettre les fichiers de moins de 1 Go dans un dossier séparé ?")
+        move_large_files_answer = prompt([move_large_files_question])
+        if move_large_files_answer['move_large_files']:
+            file_sorter.move_large_files()
+        file_sorter.create_extension_folders()
+        file_sorter.move_files()  # Déplace toujours les fichiers dans des dossiers par extension
 
-    # Déplacer les fichiers de moins de 1 Go vers "A Mettre A Jour"
-    if update_folder:
-        file_sorter.move_large_files()
-
-    # Créer des dossiers pour chaque extension
-    file_sorter.create_extension_folders()
-
-    # Déplacer les fichiers dans les dossiers correspondants
-    file_sorter.move_files()
+    elif "2. Donner la liste des fichiers" in answers['action']:
+        file_types_input = input("Entrez l'extension des fichiers à filtrer (séparées par des espaces, appuyez sur Entrée pour traiter toutes les extensions) : ")
+        file_types = file_types_input.split() if file_types_input else None
+        file_sorter = FileSorter(source_dir)
+        file_sorter.filter_files_by_extension(file_types)
 
 if __name__ == "__main__":
     main()
